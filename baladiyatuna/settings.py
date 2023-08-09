3d1a9387d8b373f2e5c407abfe4db31a0da04259
Lasheb Mohamed Amine <71101585+aminelasheb@users.noreply.github.com>
2023-08-09 18:55:52 +0100

    s

--- baladiyatuna/settings.py
+++ baladiyatuna/settings.py
@@ -153,7 +153,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'baladiya',
         'USER': 'postgres',
-        'PASSWORD': 'assiamdn2001',
+        'PASSWORD': '1605',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
