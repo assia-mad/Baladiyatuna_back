e000fa266f7721ede2c5af5753f03874177fd532
Lasheb Mohamed Amine <71101585+aminelasheb@users.noreply.github.com>
2023-08-21 19:44:00 +0100

    historique v2

--- baladiya/views.py
+++ baladiya/views.py
@@ -16,6 +16,7 @@ from django.db.models.functions import TruncDate
 import datetime
 from rest_framework.exceptions import ParseError
 from django.utils.dateparse import parse_date
+import django_filters
 
 
 
@@ -363,14 +364,20 @@ class VisiteView(viewsets.ModelViewSet):
     search_fields = ["owner__id", "title", "description", "created_at","liked_by","localisation"]
     ordering_fields = ["created_at"]
 
+class HistoriqueFilter(django_filters.FilterSet):
+    commune = django_filters.NumberFilter(field_name='commune')
 
+    class Meta:
+        model = Historique
+        fields = ['commune']
 class HistoriqueListCreateView(generics.ListCreateAPIView):
     queryset = Historique.objects.all()
     serializer_class = HistoriqueSerializer
     pagination_class = CustomPagination
     permission_classes = [IsAuthenticated]
+    filterset_class = HistoriqueFilter
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    ordering_fields = ["date"]
+    ordering_fields = ["date","commune"]
 
     def perform_create(self, serializer):
         user = self.request.user
@@ -381,5 +388,6 @@ class HistoriqueRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = HistoriqueSerializer
     pagination_class = CustomPagination
     permission_classes = [IsAuthenticated]
+    filterset_class = HistoriqueFilter
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    ordering_fields = ["date"]
+    ordering_fields = ["date","commune"]
