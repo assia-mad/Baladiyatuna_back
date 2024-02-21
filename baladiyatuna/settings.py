4503e688d008f83f4c88a753c7556995827034b1
assia-mad <as.madaniyousfi@esi-sba.dz>
2024-02-21 23:05:56 +0100

    changing smtp email app

--- baladiyatuna/settings.py
+++ baladiyatuna/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,8 +111,11 @@ REST_AUTH_PW_RESET_USE_SITES_DOMAIN = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "baladiyatuna@gmail.com"
-EMAIL_HOST_PASSWORD = "anlziprhpwivmubw" 
+from decouple import config
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+ 
 EMAIL_PORT = 587
 
 REST_AUTH_REGISTER_SERIALIZERS = {
