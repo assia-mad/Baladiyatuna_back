79e66a6ab97a93396723487df59fb5e288189e31
assia-mad <as.madaniyousfi@esi-sba.dz>
2024-02-21 22:32:14 +0100

    modifiying email smtp

--- baladiyatuna/settings.py
+++ baladiyatuna/settings.py
@@ -110,8 +110,8 @@ REST_AUTH_PW_RESET_USE_SITES_DOMAIN = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "aldys06e@gmail.com"
-EMAIL_HOST_PASSWORD = "yjrdrtdpvrqgfgsv"
+EMAIL_HOST_USER = "baladiyatuna@gmail.com"
+EMAIL_HOST_PASSWORD = "anlziprhpwivmubw" 
 EMAIL_PORT = 587
 
 REST_AUTH_REGISTER_SERIALIZERS = {
